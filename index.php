1009812d0894ee0d907bc48f9e0eada81232a688
ABS <antok.benedek@gmail.com>
2019-04-18 19:45:27 +0200

    add fancy graphics to home page

--- index.php
+++ index.php
@@ -4,9 +4,39 @@
 <body>
   <?php include 'menu.html'; ?>
   <div class="container main-content">
-    <div class="jumbotron">
-      <h1>Program nyilvántartás</h1>
-      <p class="lead">Lyukkártyás programok adatbázisa</p>
+    <div class="punchwrap">
+      <div class="jumbotron text-light">
+        <h1>Lyukkártyás program nyilvántartás</h1>
+        <div class="punch"></div>
+      </div>
     </div>
   </div>
+
+  <script>
+    const generate = () => {
+      let punch = document.getElementsByClassName('punch')[0];
+      punch.innerHTML = "";
+
+      for (let j = 0; j < 10; j++) {
+
+        let line = document.createElement('div');
+        line.classList.add('line');
+
+        for (let i = 0; i < 50; i++) {
+            let number = document.createElement('div');
+            number.innerText = j;
+            number.classList.add(Math.random() < 0.8 ? 'number' : 'hole');
+            line.append(number)
+        }
+
+        punch.append(line);
+      }
+    }
+
+    generate();
+    setInterval(() => {
+      generate();
+    }, 2*1000);
+  </script>
+
 <?php include 'common_footer.html'; ?>
